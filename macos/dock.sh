d289785062f24a9a74a8e6627edac8a0e469f6c3
Richard Bates <rich.bates@protonmail.com>
2022-01-26 19:38:18 +0000

    Update dock.sh

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/System/Applications/Mail.app"
-dockutil --no-restart --add "/System/Applications/Calendar.app"
-dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
+dockutil --no-restart --add "/Applications/Finder.app"
+dockutil --no-restart --add "/Applications/Notes.app"
 dockutil --no-restart --add "/System/Applications/System Preferences.app"
+dockutil --no-restart --add "/Applications/1Password 7.app"
+dockutil --no-restart --add "/Applications/iTerm.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
+dockutil --no-restart --add "/Applications/Github Desktop.app"
+dockutil --no-restart --add "/Applications/Firefox.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 
 killall Dock
